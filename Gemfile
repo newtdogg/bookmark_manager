3fc0be3ae93d235699e660f076142a03e81fc017
charles <charlesemery15@gmail.com>
2017-12-11 17:56:57 +0000

    Created a database

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+ruby '2.4.2'
+
+gem 'rspec'
+gem 'rubocop'
+gem 'capybara'
+gem 'sinatra'
+gem 'pry'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'shotgun'
+gem 'selenium-webdriver'
---
+++ Student_model_setup.rb
@@ -0,0 +1,25 @@
+require 'dm-migrations'
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, "postgres://localhost/database_play")
+
+class Student
+  include DataMapper::Resource
+  property :id,     Serial
+  property :name,   String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+Student.create(name: 'Rhys', id: 0)
+Student.create(name: 'Charles', id: 2)
+Student.create(name: 'Ted', id: 69)
+student = Student.first(name: 'Ted')
+student.destroy
+Student.create(name: 'Charles', id: 3)
+Student.create(name: 'Charles', id: 1)
+student = Student.first(name: 'Charles')
+student.destroy
