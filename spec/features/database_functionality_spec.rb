2efec5d18d33b5c06c5e249d8a4c419d11673692
charles <charlesemery15@gmail.com>
2017-12-12 17:19:16 +0000

    Just started Heroku

--- spec/features/database_functionality_spec.rb
+++ spec/features/database_functionality_spec.rb
@@ -1,12 +1,12 @@
-# feature 'View Homepage' do
-#   scenario 'should display a list of links' do
-#     Link.create(url: 'https://en.wikipedia.org/wiki/Albion', title: 'Wikipedia Albion')
-#     visit ('/links')
-#     # this checks if the link works with a status code of 200 ok
-#     expect(page.status_code).to eq 200
-#
-#     within 'ul#links' do
-#       expect(page).to have_content('Albion')
-#     end
-#   end
-# end
+feature 'View Homepage' do
+  scenario 'should display a list of links' do
+    Link.create(url: 'https://en.wikipedia.org/wiki/Albion', title: 'Wikipedia Albion')
+    visit ('/links')
+    # this checks if the link works with a status code of 200 ok
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Albion')
+    end
+  end
+end
